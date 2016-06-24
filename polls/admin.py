b2296884e53b692d4d47bbed53b6ced46c64f395
Jonathan Orozco <jorozco@tevora.com>
2016-06-24 16:28:07 -0700

    minor changes to html files and added css

--- polls/admin.py
+++ polls/admin.py
@@ -13,7 +13,7 @@ class QuestionAdmin(admin.ModelAdmin):
 		('Date information',{'fields': ['pub_date']}),
 	]
 	inlines = [ChoiceInline]
-
-
-
+	search_fields = ['question_text']
+	list_display = ('question_text', 'pub_date', 'was_published_recently')
+	list_filter = ['pub_date']
 admin.site.register(Question, QuestionAdmin)
